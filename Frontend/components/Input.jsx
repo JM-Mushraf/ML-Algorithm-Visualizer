bfafc3066c243a6c869fe72f0de6c3569873edd9
JM-Mushraf <mushraf1786@gmail.com>
2025-03-04 16:57:47 +0530

    Basic Front2

--- Frontend/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-// components/Input.jsx
-import React from "react";
-import "./Input.css";
-
-const Input = ({ type = "text", placeholder, onChange }) => {
-  return <input className="input" type={type} placeholder={placeholder} onChange={onChange} />;
-};
-
-export default Input;
--- Frontend/components/card.jsx
+++
@@ -1,9 +0,0 @@
-// components/Card.jsx
-import React from "react";
-import "./Card.css"; // Import CSS for styling
-
-const Card = ({ children }) => {
-  return <div className="card">{children}</div>;
-};
-
-export default Card;
